d2931035d57c378a0e124d80d9a1a0530e61f7cd
KyleBergeron <kyle.bergeron1994@yahoo.com>
2017-11-08 17:47:03 -0500

    .

---
+++ app/src/main/java/com/aymanbagabas/barpop/AmazonServiceProvider.java
@@ -0,0 +1,27 @@
+package com.aymanbagabas.barpop;
+
+import android.net.NetworkRequest;
+import android.os.Build;
+import android.support.v4.*;
+
+/**
+ * Created by matt on 11/8/2017.
+ */
+
+ public class AmazonServiceProvider extends ServiceProvider {
+    final String POOL_ID = "us-east-2_vi5leiwG4";
+    final String CLIENT_ID = "2vh33a0af73l6k6p85g30era40";
+    final String CLIENT_SECRET = "s11pi6l420b0npg6vp61c7dqkcg2f1sdnkas7q4dfgdjsmgrac9";
+     AmazonServiceProvider(String clientId,String CLIENT_ID, String CLIENT_SECRET){
+
+    }
+    @Override
+    public void login() {
+
+    }
+
+    @Override
+    public void signUp() {
+
+    }
+}
---
+++ app/src/main/java/com/aymanbagabas/barpop/ServiceProvider.java
@@ -0,0 +1,6 @@
+package com.aymanbagabas.barpop;
+
+abstract public class  ServiceProvider {
+    abstract public void login();
+    abstract public void signUp();
+}
